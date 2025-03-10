ad005a6bd79216a194d04b5bb7bcbf26419fe235
moabzomg <wumantsun@yahoo.com.hk>
2025-03-10 20:48:36 +0000

    update

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
         My Portfolio
       </h1>
       <p className="mb-4">
-        {`I'm a perfectionist who always strive for success in every situation. Back in Hong Kong, I moved recently to UK and starting a new life here. I am going to adapt to the new environment through landing my first job.`}
+        {`I'fhjgss in every situation. I moved recently to UK and starting a new life here. I am going to adapt to the new environment through landing my first job.`}
       </p>
     </section>
   )

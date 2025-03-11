b3b513de998c5c7c1e2a8df5f469590abff9f928
moabzomg <wumantsun@yahoo.com.hk>
2025-03-11 14:40:03 +0000

    mobile enviroment update

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -20,24 +20,20 @@ const navItems = {
 
 export function Navbar() {
   return (
-    <aside className="-ml-[8px] mb-16 tracking-tight">
-      <div className="lg:sticky lg:top-20">
-        <nav
-          className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
-          id="nav"
-        >
-          <div className="flex flex-row space-x-0 pr-10">
-            {Object.entries(navItems).map(([path, { name }]) => {
-              return (
-                <Link
-                  key={path}
-                  href={path}
-                  className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
-                >
-                  {name}
-                </Link>
-              );
-            })}
+    <aside className="mb-16 tracking-tight">
+      <div className="px-4 sm:px-6 lg:px-8">
+        <nav className="flex justify-between items-center">
+          {/* Navigation links */}
+          <div className="flex space-x-8 flex-nowrap">
+            {Object.entries(navItems).map(([path, { name }]) => (
+              <Link
+                key={path}
+                href={path}
+                className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 py-2 px-4 text-lg font-medium"
+              >
+                {name}
+              </Link>
+            ))}
           </div>
         </nav>
       </div>
